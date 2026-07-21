d5ad7c5d7bdfeddd9c39f1fabf940a519a90bf20
analyticflow <mahekyadav30@gmail.com>
2026-07-21 19:18:36 +0530

    Complete Stored Procedure with revision questions

--- day15_stored_procedures.sql
+++ day15_stored_procedures.sql
@@ -92,3 +92,106 @@ DROP PROCEDURE HighSalaryEmployees;
 
 SHOW PROCEDURE STATUS
 WHERE Db = 'school_db';
+
+
+-- Show employees whose salary is more than ₹50,000.
+
+DELIMITER //
+
+CREATE PROCEDURE EmployeesAbove50000()
+BEGIN
+    SELECT *
+    FROM employees_procedure
+    WHERE salary > 50000;
+END //
+
+DELIMITER ;
+
+CALL EmployeesAbove50000();
+
+
+-- Returns employees earning ₹50,000 or more, sorted from highest salary to lowest salary.
+DELIMITER //
+
+CREATE PROCEDURE EmployeesBySalary()
+BEGIN
+    SELECT *
+    FROM employees_procedure
+    WHERE salary >= 50000
+    ORDER BY salary DESC;
+END //
+
+DELIMITER ;
+
+CALL EmployeesBySalary();
+
+
+-- Show all IT employees sorted by salary in descending order.
+
+DELIMITER //
+
+CREATE PROCEDURE ITEmployeesDescending()
+BEGIN
+    SELECT *
+    FROM employees_procedure
+    WHERE department='IT'
+    ORDER BY salary DESC;
+END //
+
+DELIMITER ;
+
+CALL ITEmployeesDescending();
+
+
+-- Show employees whose salary is between ₹45,000 and ₹60,000.
+
+DELIMITER //
+
+CREATE PROCEDURE SalaryBetween45k60k()
+BEGIN
+    SELECT *
+    FROM employees_procedure
+    WHERE salary BETWEEN 45000 AND 60000;
+END //
+
+DELIMITER ;
+
+CALL SalaryBetween45k60k();
+
+
+-- Show the total number of employees in each department.
+
+DELIMITER //
+
+CREATE PROCEDURE EmployeeCountDepartmentWise()
+BEGIN
+    SELECT
+        department,
+        COUNT(*) AS Total_Employees
+    FROM employees_procedure
+    GROUP BY department;
+END //
+
+DELIMITER ;
+
+CALL EmployeeCountDepartmentWise();
+
+
+-- Show employees whose salary is greater than the average salary.
+
+DELIMITER //
+
+CREATE PROCEDURE EmployeesAboveAverageSalary()
+BEGIN
+    SELECT *
+    FROM employees_procedure
+    WHERE salary >
+    (
+        SELECT AVG(salary)
+        FROM employees_procedure
+    );
+END //
+
+DELIMITER ;
+
+CALL EmployeesAboveAverageSalary();
